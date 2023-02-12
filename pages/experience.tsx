7c0c1229fad6690f1e1b79ecd59eb3d026e71d72
Harivansh-coder <harivanshthakur8@gmail.com>
2023-02-12 23:09:49 +0530

    done page routing

---
+++ pages/experience.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ContainerBlock from "../components/ContainerBlock";
+import Experience from "../components/Experience";
+export default function experience() {
+  return (
+    <ContainerBlock title="Experience">
+      <Experience />
+    </ContainerBlock>
+  );
+}
