14da0685e73e1d53d7a2f7018f10b6631dbcc3df
Harivansh-coder <harivanshthakur8@gmail.com>
2023-02-12 23:06:42 +0530

    created required components

---
+++ components/AboutMe.tsx
@@ -0,0 +1,208 @@
+import React from "react";
+import userData from "../constants/data";
+
+export default function AboutMe() {
+  return (
+    <section className="bg-white dark:bg-gray-800">
+      <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
+        <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
+          About Me.
+        </h1>
+      </div>
+      <div className="bg-[#F1F1F1] -mt-10 dark:bg-gray-900">
+        <div className="text-container max-w-6xl mx-auto pt-20">
+          <p
+            className="leading-loose text-2xl md:text-3xl   mx-4"
+            style={{ lineHeight: "3rem" }}
+          >
+            {userData.about.title}
+            {/* <a
+              className="bg-red-500 rounded-md px-2 py-1 text-white"
+              href={userData.about.currentProjectUrl}
+            >
+              {userData.about.currentProject} ✈️
+            </a> */}
+          </p>
+        </div>
+      </div>
+      <div className="bg-[#F1F1F1] dark:bg-gray-900 px-4">
+        <div className="pt-20 grid grid-cols-1 md:grid-cols-3 max-w-6xl mx-auto gap-y-20 gap-x-20">
+          {/* Social Buttons */}
+          <div className="inline-flex flex-col">
+            <div>
+              <h1 className="text-xl font-semibold text-gray-700 dark:text-gray-200">
+                Contact
+              </h1>
+              <p className="text-lg text-gray-500 mt-4 dark:text-gray-300">
+                Here is my{" "}
+                <a
+                  href={`mailto:${userData.email}`}
+                  className="text-gray-800 border-b-2 border-gray-800 dark:border-gray-300 font-bold dark:text-gray-300"
+                >
+                  mail
+                </a>{" "}
+                , in case any query.
+              </p>
+            </div>
+            <div className="mt-8">
+              <h1 className="text-xl font-semibold text-gray-700 dark:text-gray-200">
+                Job Opportunities
+              </h1>
+              <p className="text-lg text-gray-500 mt-4 dark:text-gray-300">
+                I'm looking for a job currently, If you see me as a good fit,
+                check my{" "}
+                <a
+                  href={userData.resumeUrl}
+                  target="__blank"
+                  className="text-gray-800 border-b-2 border-gray-800 dark:border-gray-300 font-bold dark:text-gray-300"
+                >
+                  CV
+                </a>{" "}
+                and I'd love to work for you.
+              </p>
+            </div>
+            {/* Social Links */}
+            <h1 className="text-xl font-semibold text-gray-700 mt-8 dark:text-gray-200">
+              Social Links
+            </h1>
+            <div className="mt-4 ml-4">
+              {/* <div className="flex flex-row justify-start items-center ">
+                <a
+                  href={userData.socialLinks.facebook}
+                  className="flex flex-row items-center space-x-4 group"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <div className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
+                    Facebook
+                  </div>
+                </a>
+              </div> */}
+              <div className="flex flex-row justify-start items-center">
+                <a
+                  href={userData.socialLinks.twitter}
+                  className="flex flex-row items-center space-x-4 group"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <div className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
+                    Twitter
+                  </div>
+                </a>
+              </div>
+              <div className="flex flex-row justify-start items-center">
+                <a
+                  href={userData.socialLinks.github}
+                  className="flex flex-row items-center space-x-4 group"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <div className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
+                    GitHub
+                  </div>
+                </a>
+              </div>
+              <div className="flex flex-row justify-start items-center">
+                <a
+                  href={userData.socialLinks.linkedin}
+                  className="flex flex-row items-center space-x-4 group"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <div className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
+                    LinkedIn
+                  </div>
+                </a>
+              </div>
+              {/* <div className="flex flex-row justify-start items-center">
+                <a
+                  href={userData.socialLinks.instagram}
+                  className="flex flex-row items-center space-x-4 group"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <div className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-28 group-hover:translate-x-0 transition duration-300"></div>
+                    Instagram
+                  </div>
+                </a>
+              </div> */}
+            </div>
+          </div>
+          {/* Text area */}
+          <div className="col-span-1 md:col-span-2">
+            {userData.about.description?.map((desc, idx) => (
+              <p
+                key={idx}
+                className="text-xl text-gray-700 mb-4 dark:text-gray-300 "
+              >
+                {desc}
+              </p>
+            ))}
+
+            <h1 className="bg-red-500 text-3xl rounded-md px-2 py-1 inline-block font-bold text-gray-50">
+              Tech Stack
+            </h1>
+            <div className="flex flex-row flex-wrap mt-8">
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/python/python.png"
+                alt="python logo"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/java/java.png"
+                className="h-20 w-20 mx-4 my-4"
+                alt="java logo"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/typescript/typescript.png"
+                className="h-20 w-20 mx-4 my-4"
+                alt="typescript logo"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png"
+                className="h-20 w-20 mx-4 my-4"
+                alt="javascript logo"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/angular/angular.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/vue/vue.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/git/git.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/firebase/firebase.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mysql/mysql.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongodb/mongodb.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,229 @@
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { useTheme } from "next-themes";
+import { useRouter } from "next/router";
+import userData from "../constants/data";
+
+export default function Navbar() {
+  const router = useRouter();
+  // console.log(router.asPath);
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <div className="max-w-6xl  mx-auto px-4 py-10 md:py-20">
+      <div className="flex  md:flex-row justify-between items-center">
+        <div className="flex flex-col">
+          <Link href="/">
+            <h1 className="font-semibold text-xl dark:text-gray-100">
+              {userData.name}
+            </h1>
+            <p className="text-base font-light text-gray-500 dark:text-gray-300">
+              {userData.designation}
+            </p>
+          </Link>
+        </div>
+
+        <div className="space-x-8 hidden md:block">
+          <Link
+            href="/about"
+            className={`text-base  ${
+              router.asPath === "/about"
+                ? "text-gray-800 font-bold dark:text-gray-400"
+                : "text-gray-600 dark:text-gray-300 font-normal "
+            }`}
+          >
+            About{" "}
+            {router.asPath === "/about" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-arrow-down inline-block h-3 w-3"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                />
+              </svg>
+            )}
+          </Link>
+          <Link
+            href="/projects"
+            className={`text-base  ${
+              router.asPath === "/projects"
+                ? "text-gray-800 font-bold dark:text-gray-400"
+                : "text-gray-600 dark:text-gray-300 font-normal "
+            }`}
+          >
+            Projects
+            {router.asPath === "/projects" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-arrow-down inline-block h-3 w-3"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                />
+              </svg>
+            )}
+          </Link>
+          <Link
+            href="/experience"
+            className={`text-base  ${
+              router.asPath === "/experience"
+                ? "text-gray-800 font-bold dark:text-gray-400"
+                : "text-gray-600 dark:text-gray-300 font-normal "
+            }`}
+          >
+            Experience{" "}
+            {router.asPath === "/experience" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-arrow-down inline-block h-3 w-3"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                />
+              </svg>
+            )}
+          </Link>
+          <Link
+            href="/contact"
+            className={`text-base  ${
+              router.asPath === "/contact"
+                ? "text-gray-800 font-bold dark:text-gray-400"
+                : "text-gray-600 dark:text-gray-300 font-normal "
+            }`}
+          >
+            Contact
+            {router.asPath === "/contact" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-arrow-down inline-block h-3 w-3"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                />
+              </svg>
+            )}
+          </Link>
+        </div>
+
+        <div className="space-x-4 flex flex-row items-center">
+          <a
+            href={userData.socialLinks.github}
+            target="_blank"
+            className="text-base font-normal text-gray-600 dark:text-gray-300"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-github h-5 w-5"
+              viewBox="0 0 496 512"
+            >
+              <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+            </svg>
+          </a>
+          <a
+            href={userData.socialLinks.linkedin}
+            target="_blank"
+            className="text-base font-normal text-gray-600 dark:text-gray-300"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-linkedin h-5 w-5"
+              viewBox="0 0 16 16"
+            >
+              <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+            </svg>
+          </a>
+          <button
+            aria-label="Toggle Dark Mode"
+            type="button"
+            className="w-10 h-10 p-3 rounded focus:outline-none"
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          >
+            {mounted && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                stroke="currentColor"
+                className="w-4 h-4 text-yellow-500 dark:text-yellow-500"
+              >
+                {theme === "dark" ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                  />
+                )}
+              </svg>
+            )}
+          </button>
+        </div>
+      </div>
+      <div className="space-x-8 block md:hidden mt-4">
+        <Link
+          href="/about"
+          className="text-base font-normal text-gray-600 dark:text-gray-300"
+        >
+          About
+        </Link>
+        <Link
+          href="/projects"
+          className="text-base font-normal text-gray-600 dark:text-gray-300"
+        >
+          Projects
+        </Link>
+        <Link
+          href="/experience"
+          className="text-base font-normal text-gray-600 dark:text-gray-300"
+        >
+          Experience
+        </Link>
+        <Link
+          href="/contact"
+          className="text-base font-normal text-gray-600 dark:text-gray-300"
+        >
+          Contact
+        </Link>
+      </div>
+    </div>
+  );
+}
