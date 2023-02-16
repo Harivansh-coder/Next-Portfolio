408612fa75428d458e152ee3f449247309770f18
Harivansh-coder <harivanshthakur8@gmail.com>
2023-02-16 22:48:08 +0530

    removed again ;)

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -24,24 +24,6 @@ export default function Contact() {
               </p>
             </header>
             <div className="icons-container inline-flex flex-col my-10">
-              {/* <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-telephone-fill h-4 w-4 text-blue-500"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
-                  />
-                </svg>
-                <p className="text-gray-700  dark:text-white font-light text-sm">
-                  {userData.phone}
-                </p>
-              </div> */}
               <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
