2223f6747cf6c1bc680fad8cebecd1195b5d9ce8
Harivansh-coder <harivanshthakur8@gmail.com>
2023-02-21 17:07:39 +0530

    generated sitemap :)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,17 +19,17 @@ export default function Hero() {
           </RainbowHighlight>
           <RainbowHighlight color={colors[1]}>
             <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
-              Designer.
+              Designer
             </h1>
           </RainbowHighlight>
           <RainbowHighlight color={colors[2]}>
             <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
-              Programmer.
+              Programmer
             </h1>
           </RainbowHighlight>
           <RainbowHighlight color={colors[3]}>
             <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
-              Writer.
+              Writer
             </h1>
           </RainbowHighlight>
         </RoughNotationGroup>
